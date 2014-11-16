676e2e84f01d93af06c94e030ef375e1aa804e77
menghong <chhaymenghong@yahoo.com>
2014-11-15 18:25:42 -0800

    add more fact. Change from FileReader to inputStream

--- app/src/main/java/menghong/funfacts/FunFactsActivity.java
+++ app/src/main/java/menghong/funfacts/FunFactsActivity.java
@@ -1,6 +1,7 @@
 package menghong.funfacts;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,15 +9,30 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class FunFactsActivity extends Activity {
-    private JsonParserOperation factsBook = new JsonParserOperation();
-    private ColorWheel colorWheel = new ColorWheel();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Set this xml as layout
         setContentView(R.layout.activity_fun_facts);
+        AssetManager assetManager = getAssets();
+        InputStream iS = null;
+        try {
+            iS = assetManager.open("funfacts.json");
+            System.out.println(iS);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final JsonParserOperation factsBook = new JsonParserOperation(iS);
+        final ColorWheel colorWheel = new ColorWheel();
+
         // R is the auto generated class containing references to all ids
         // Required casting coz findView returns super class View
         final TextView factLabel = (TextView)findViewById(R.id.factTextView);
@@ -38,10 +54,9 @@ public class FunFactsActivity extends Activity {
         // Add onClickListener to button
         // It takes in a view Listener
         showFactButton.setOnClickListener(listener);
-        // for outputting toast message
-//        String message = "Yay! Out Activity was created!";
+        //for outputting toast message
+//        String message = System.getProperty("user.dir");
 //        Toast welcomeToast = Toast.makeText(this, message, Toast.LENGTH_LONG);
 //        welcomeToast.show();
-
     }
 }
--- app/src/main/java/menghong/funfacts/JsonParserOperation.java
+++ app/src/main/java/menghong/funfacts/JsonParserOperation.java
@@ -1,45 +1,49 @@
 package menghong.funfacts;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Random;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 // This class is used to parse JSON text to
 public class JsonParserOperation {
-    private static final String filePath = "C:\\Users\\menghong\\Desktop\\AllProjects\\Fun-Facts\\app\\src\\main\\res";
     private ArrayList<String> factsBook;
     private int factsBookSize;
-    public JsonParserOperation() {
+    private InputStream iS;
+    public JsonParserOperation(InputStream iS) {
+        this.iS = iS;
         factsBook = generateFactBook();
         factsBookSize = factsBook.size();
+
     }
+    // Create FactBook
     public ArrayList<String> generateFactBook() {
         ArrayList<String> listOfFacts = new ArrayList<String>();
         try {
-            // Read Json file and turn it into Json object
-            FileReader reader = new FileReader(filePath);
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
-
-            // get information from the Json object
-            JSONArray listFactObject = (JSONArray) jsonObject.get("results");
-            for (int i = 0; i < listFactObject.length(); i++) {
-                JSONObject eachFactObject = listFactObject.getJSONObject(i);
-                String eachFact = (String) eachFactObject.get("facts");
-                listOfFacts.add(eachFact);
+            BufferedReader streamReader = new BufferedReader(new InputStreamReader(iS));
+            StringBuilder file = new StringBuilder();
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null) {
+                file.append(inputStr);
             }
-
+            // Read Json file and turn it into Json object
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = new JSONObject(file.toString());
+            // get information from the Json object
+            JSONArray listFactObject = (JSONArray) jsonObject.get("data");
+            for (int i = 0; i < listFactObject.length(); i++) {
+                JSONObject dataObject = (JSONObject) listFactObject.get(i);
+                JSONArray factArray = (JSONArray)dataObject.get("facts");
+                String eachFact = (String) factArray.get(0);
+                 listOfFacts.add(eachFact);
+            }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
